1661f59f9de60c7c3c4e58eee472ace4a44c4dd3
handsome_code_boy <1376848042@qq.com>
2026-02-26 20:10:38 +0800

    feat

---
+++ STL容器/stack.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    stack<int> s;
+    for(int i = 0;i < 10;i++)
+    {
+        s.push(i);
+    }
+    while(!s.empty())
+    {
+        cout << s.top() << ' ';
+        s.pop();
+    }
+    cout << endl;
+    return 0;
+}
