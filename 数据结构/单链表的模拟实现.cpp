877fa304f2ba7544943bf122bca02d4d8dacaf23
handsome_code_boy <1376848042@qq.com>
2026-02-26 18:53:29 +0800

    fixed

--- 数据结构/单链表的模拟实现.cpp
+++ 数据结构/单链表的模拟实现.cpp
@@ -9,7 +9,7 @@ int head,id;
 void initial()
 {
     head = 0;
-    list_next[head] = -1;
+    list_next[head] = 0;
     id = 0;
 }
 //头插
@@ -19,6 +19,30 @@ void push_front(elem_type e)
     list_data[id] = e;
     list_next[id] = list_next[head];
     list_next[head] = id;
+}
+//头删
+void pop_front(void)
+{
+    list_next[head] = list_next[list_next[head]];
+}
+//按位查找
+int find(int pos)
+{
+    int p = list_next[head];
+    for(int i = 0;i < pos - 1;i++)
+    {
+        p = list_next[p];
+    }
+    return p;
+}
+//在任意元素位置之后插入数据
+void insert(int pos,elem_type e)
+{
+    int p = find(pos);
+    id++;
+    list_data[id] = e;
+    list_next[id] = list_next[p];
+    list_next[p] = id;
 }
 //遍历
 void print_list(void)
@@ -35,6 +59,11 @@ int main()
     push_front(0);
     push_front(2);
     push_front(5);
+    push_front(2);
+    print_list();
+    pop_front();
+    print_list();
+    insert(1,8);
     print_list();
     return 0;
 }
