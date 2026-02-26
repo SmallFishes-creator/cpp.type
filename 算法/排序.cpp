6672cbd3733170793d78f6e7747eaf94f57ae18e
handsome_code_boy <1376848042@qq.com>
2026-02-26 22:12:21 +0800

    feat

---
+++ 算法/排序.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//插入排序
+void insert_sort(int* arr,int sz)
+{
+    for(int i = 1;i < sz;i++)
+    {
+        int key = arr[i];
+    }
+}
+int main()
+{
+    int arr[] = {4,2,4,5,1};
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    return 0;
+}
