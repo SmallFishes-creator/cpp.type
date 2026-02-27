afdc2e1d9ad2b122856cc96643ebc7e09a9f2992
handsome_code_boy <1376848042@qq.com>
2026-02-27 17:18:36 +0800

    feat

---
+++ 数据结构/vector数组存储树.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+const int N = 1e5 + 10;
+int n;
+vector<int> edges[N];
+int main()
+{
+    cin >> n;
+    for(int i = 0;i < n;i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        edges[a].push_back(b);
+        edges[b].push_back(a);
+    }
+    return 0;
+}
