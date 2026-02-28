3ec437772a78d8daff1a7bf92e4b0cbf79814991
handsome_code_boy <1376848042@qq.com>
2026-02-28 15:10:03 +0800

    feat

---
+++ 数据结构/链式前向星存储树.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+const int N = 1e5 + 10;
+int h[N],e[N * 2],ne[N * 2],id = 0,n = 0;
+void add(int a,int b)
+{
+    id++;
+    e[id] = a;
+    ne[id] = h[b];
+    h[b] = id;
+}
+int main()
+{
+    cin >> n;
+    for(int i = 0;i < n;n++)
+    {
+        int a,b;
+        cin >> a >> b;
+        add(a,b);
+        add(b,a);
+    }
+    return 0;
+}
