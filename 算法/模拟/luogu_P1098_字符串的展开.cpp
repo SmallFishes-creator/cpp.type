819f0b85ea4e3f6a180d5e51f9b9b6b9fdebbd81
handsome_code_boy <1376848042@qq.com>
2026-03-19 10:45:41 +0800

    feat

--- 算法/模拟/luogu_P1098_字符串的展开.cpp
+++ 算法/模拟/luogu_P1098_字符串的展开.cpp
@@ -3,10 +3,41 @@
 #include <cctype>
 #include <string>
 using namespace std;
+int p1,p2,p3;
+string s;
+void finish(int& i)
+{
+    string tmp;
+    if(p3 == 1)
+    {
+        for(char k = s[i - 1] + 1;k <= s[i + 1] - 1;k++)
+        {
+            for(int j = 0;j < p2;j++)
+                tmp.push_back(k);
+        }
+    }
+    else
+    {
+        for(char k = s[i + 1] - 1;k >= s[i - 1] + 1;k--)
+        {
+            for(int j = 0;j < p2;j++)
+                tmp.push_back(k);
+        }
+    }
+    if(p1 == 2)
+        transform(tmp.begin(),tmp.end(),tmp.begin(),::toupper);
+    if(p1 == 3)
+    {
+        for(int i = 0;i < tmp.size();i++)
+        {
+            tmp[i] = '*';
+        }
+    }
+    s.replace(i,1,tmp);
+    i += tmp.size() - 1;
+}
 int main()
 {
-    string s;
-    int p1,p2,p3;
     cin >> p1 >> p2 >> p3;
     cin >> s;
     for(int i = 0;i < s.size();i++)
@@ -15,34 +46,16 @@ int main()
             continue;
         if(s[i] == '-' && s[i - 1] >= 'a' && s[i - 1] < s[i + 1] && s[i + 1] <= 'z')
         {
-            string tmp;
-            if(p3 == 1)
-            {
-                for(char i = s[i - 1] + 1;i <= s[i + 1] - 1;i++)
-                {
-                    for(int j = 0;j < p2;j++)
-                        tmp.push_back(i);
-                }
-            }
-            else
-            {
-                for(char i = s[i + 1] - 1;i >= s[i - 1] + 1;i--)
-                {
-                    for(int j = 0;j < p2;j++)
-                        tmp.push_back(i);
-                }
-            }
-            if(p1 == 2)
-                transform(tmp.begin(),tmp.end(),tmp.begin(),::toupper);
-            if(p2 == 3)
-            {
-                for(int i = 0;i < tmp.size();i++)
-                {
-                    tmp[i] = '*';
-                }
-            }
-
+            finish(i);
         }
+        if(s[i] == '-' && s[i - 1] >= '0' && s[i - 1] < s[i + 1] && s[i + 1] <= '9')
+        {
+            finish(i);
+        }
+    }
+    for(char e : s)
+    {
+        cout << e;
     }
     return 0;
 }
