849ce3e1c7613cb0e361637b1d3edd7d6576ff45
handsome_code_boy <1376848042@qq.com>
2026-03-26 19:14:13 +0800

    feat

---
+++ 小练习/字符整行读取.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    string s;
+    getline(cin,s);
+    cout << s << endl;
+    return 0;
+}
---
+++ 算法/前缀和/luogu_P2280_激光炸弹.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstring>
+#include <climits>
+using namespace std;
+const int N = 5e3 + 10;
+int arr[N][N];
+int sum[N][N];
+int main()
+{
+    memset(arr,0,sizeof(arr));
+    memset(sum,0,sizeof(sum));
+    int n,m;
+    cin >> n >> m;
+    int mx = 0;
+    int my = 0;
+    while(n--)
+    {
+        int x,y,v;
+        cin >> x >> y >> v;
+        x++;
+        y++;
+        if(x > mx)
+            mx = x;
+        if(y > my)
+            my = y;
+        arr[x][y] += v;
+    }
+    if(mx == 0)mx = 1;
+    if(my == 0)my = 1;
+    for(int i = 1;i <= mx;i++)
+    {
+        for(int j = 1;j <= my;j++)
+        {
+            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
+        }
+    }
+    int Max = INT_MIN;
+    if(m > mx || m > my)
+        Max = sum[mx][my];
+    else
+    {
+        for(int i = 1;i <= mx - m + 1;i++)
+        {
+            for(int j = 1;j <= my - m + 1;j++)
+            {
+                int x1 = i;
+                int y1 = j;
+                int x2 = i + m - 1;
+                int y2 = j + m - 1;
+                int total = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] +sum[x1 - 1][y1 - 1];
+                if(total > Max)
+                    Max = total;
+            }
+        }
+    }
+    cout << Max;
+    return 0;
+}
