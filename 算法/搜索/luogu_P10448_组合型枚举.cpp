e843e008c23547aa8522014b505c34037fdbb239
handsome_code_boy <1376848042@qq.com>
2026-05-05 13:26:24 +0800

    feat

---
+++ 算法/搜索/luogu_P10448_组合型枚举.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,m;
+vector<int> path;
+void dfs(int begin)
+{
+    if(path.size() == m)
+    {
+        for(int e : path)
+            cout << e << ' ';
+        cout << endl;
+        return;
+    }
+    for(int i = begin;i <= n;i++)
+    {
+        path.push_back(i);
+        dfs(i + 1);
+        path.pop_back();
+    }
+}
+int main()
+{
+    cin >> n >> m;
+    dfs(1);
+    return 0;
+}
