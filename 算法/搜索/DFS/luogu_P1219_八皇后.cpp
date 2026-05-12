8032deda2e3af4e6cd99a6f8f246811eebd34b64
handsome_code_boy <1376848042@qq.com>
2026-05-12 19:08:14 +0800

    feat

---
+++ 算法/搜索/DFS/luogu_P1219_八皇后.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int n;
+int record[14];
+void dfs(int count)
+{
+    for(int i = 1;i <= n;i++)
+    {
+        if(record[i] == 1)
+            continue;
+        
+    }
+}
+int main()
+{
+    cin >> n;
+    dfs(1);
+    return 0;
+}
