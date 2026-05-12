ab4799e82346e9d83ff2085429d4657ddf69802b
handsome_code_boy <1376848042@qq.com>
2026-05-12 19:08:21 +0800

    feat

--- 算法/搜索/DFS/luogu_P1219_八皇后.cpp
+++ 算法/搜索/DFS/luogu_P1219_八皇后.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
 using namespace std;
 int n;
-int record[14];
+int idx[14];
+int total;
 void dfs(int count)
 {
+    static int tmp = 3;
     for(int i = 1;i <= n;i++)
     {
-        if(record[i] == 1)
+        int flag = 1;
+        for(int j = 1;j < count;j++)
+        {
+            if(idx[j] == i || abs(count - j) == abs(i - idx[j]))
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if(flag == 0)
             continue;
-        
+        idx[count] = i;
+        if(count >= n)
+        {
+            total++;
+            if(tmp > 0)
+            {
+                for(int k = 1;k <= n;k++)
+                    cout << idx[k] << ' ';
+                cout << endl;
+                tmp--;
+            }
+            return;
+        }
+        dfs(count + 1);
+        idx[count] = 0;
     }
 }
 int main()
 {
     cin >> n;
     dfs(1);
+    cout << total << endl;
     return 0;
 }
